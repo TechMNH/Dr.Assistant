011d3fb4b49d71fb39dd22feee63a46a88ca10e8
Md Nasirul Haque <talk2lurisan@gmail.com>
2020-04-04 21:55:49 +0530

    update in deployment auto code (#4)

--- build/build-deploy.js
+++ build/build-deploy.js
@@ -6,25 +6,23 @@ console.log('Build Deploy Process Initiated...');
 const dirname = path.join(__dirname, '../');
 const angularSource = path.join(dirname, 'angular-src', 'assistant');
 // const cordovaSource = path.join(dirname, 'Cordova');
-const firebaseSource = path.join(dirname, 'firebase', 'public');
 // const apkSource = path.join(cordovaSource, 'platforms', 'android', 'app', 'build', 'outputs', 'apk', 'debug');
 
 const angularBuildCommand = 'cd ' + angularSource + ' && npm run build';
 // const cordovaBuildCommand = 'cd ' + cordovaSource + ' && cordova build';
-const firebaseBuildCommand = 'cd ' + angularSource + ' && npm run deploy';
+const firebaseDeployCommand = 'cd ' + dirname + ' && git checkout master && git pull && ' + 'cd ' + angularSource + ' && npm run deploy';
 // const gitPushCommand = 'cd ' + dirname + ' && git add . && git commit -m "Dr.Assistant auto commit" && git push';
 
-const appDestination = path.join(dirname, 'App');
+// const appDestination = path.join(dirname, 'App');
 
 exec(angularBuildCommand, err => {
     if (!err) {
         console.log('Angular Build Complete');
         // copy.sync(path.join(angularSource, 'dist'), path.join(cordovaSource));
-        copy.sync(path.join(angularSource, 'dist'), path.join(firebaseSource));
         // exec(cordovaBuildCommand, err => {
             // if (!err) {
                 // console.log('Cordova APK Build Complete');
-                exec(firebaseBuildCommand, err => {
+                exec(firebaseDeployCommand, err => {
                     if (!err) {
                         console.log('Firebase Deploy Complete');
                         // copy.sync(apkSource, appDestination);
