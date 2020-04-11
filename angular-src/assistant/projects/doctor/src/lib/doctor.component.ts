7f670d94404c6cc8dd0efdbe29b8ee599780ea20
Lurisan <talk2lurisan@gmail.com>
2020-04-11 19:03:57 +0530

    improved home page, added button to issue presp

--- angular-src/assistant/projects/doctor/src/lib/doctor.component.ts
+++ angular-src/assistant/projects/doctor/src/lib/doctor.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-doctor',
-  template: `
-    <p>
-      doctor works!
-    </p>
-  `,
-  styles: [
-  ],
+  templateUrl: './doctor.component.html',
+  styles: ['./doctor.component.scss'],
 })
 export class DoctorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  issuePrescription() {
+    this.router.navigateByUrl('/prescription');
+  }
 }
